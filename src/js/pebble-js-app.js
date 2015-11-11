82928187a93202594e9d0f1dc4e07ffc15f6876f
chelaz <chelaz@users.noreply.github.com>
2015-11-11 12:59:58 +0100

    corrected index send problem in JS

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -19,16 +19,31 @@ Pebble.addEventListener('ready',
 // Listen for when an AppMessage is received
 Pebble.addEventListener('appmessage',
   function(e) {
-    // 
+    var ComID = e.payload['FHEM_COM_ID_KEY'];
+    console.log('Received ID: ' + ComID);   
+    
     if (e.payload['FHEM_URL_KEY']) {
       console.log('Received URL: ' + e.payload['FHEM_URL_KEY']);
       var response = HTTPGET(e.payload['FHEM_URL_KEY']);
       console.log('Received response: ' + response);
-      if (response != null)
-	Pebble.sendAppMessage({ 'FHEM_RESP_KEY'   :  'success' },
-			      { 'FHEM_COM_ID_KEY' :   e.payload['FHEM_COM_ID_KEY'] });
-      else
-	Pebble.sendAppMessage({ 'FHEM_RESP_KEY' :  'not connected' });  
+      
+      var dict;
+      if (response != null) {
+        dict = { 'FHEM_RESP_KEY'   :  'success',
+                 'FHEM_COM_ID_KEY' :   e.payload['FHEM_COM_ID_KEY'] };
+      } else {
+        dict = { 'FHEM_RESP_KEY'   :  'not connected',
+                 'FHEM_COM_ID_KEY' :   e.payload['FHEM_COM_ID_KEY'] };        
+      }
+     
+      Pebble.sendAppMessage(dict,
+        function(e) {
+          console.log('Send successful.');
+        },
+        function(e) {
+          console.log('Send failed!');
+        }
+      );
       
     } else {
       console.log('Received URL not known');
